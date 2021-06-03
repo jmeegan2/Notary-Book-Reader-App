baad6fa1cd8bfcc43b583f9da6eee18aded6ddc2
James Meegan <jmeegan2@owlmail.harford.edu>
2021-06-03 17:14:59 -0400

    added books and js file with logic

--- SearchString.js
+++ SearchString.js
@@ -1,5 +1,29 @@
 //Load a book from disk
 function loadBook(filename,displayName){
     let currentBook = "";
-    let url = 
+    let url = "books/" + filename;
+
+    //reset our UI
+    document.getElementById("fileName").innerHTML = displayName;
+    document.getElementById("searchstat").innerHTML = "";
+    document.getElementById("keyword").value = "";
+
+    //Create a server request to load our book
+    var xhr = new XMLHttpRequest(); //Used to stream back text file
+    xhr.open("GET", url, true );
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState == 4 && xhr.status == 200){
+        currentBook = xhr.responseText;
+
+        document.getElementById("fileContent").innerHTML = currentBook;
+
+        var elmnt=document.getElementById("fileContent");
+        elmnt.scrollTop= 0;
+
+        }
+
+    };
+
 }
