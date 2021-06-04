54c47f6b8811468fce5703bfa1d53b449e2adeca
James Meegan <jmeegan2@owlmail.harford.edu>
2021-06-04 00:04:19 -0400

    it was weird my project was glitching

--- SearchString.js
+++ SearchString.js
@@ -17,6 +17,9 @@ function loadBook(filename,displayName){
         if(xhr.readyState == 4 && xhr.status == 200){
         currentBook = xhr.responseText;
 
+        // remove line breaks and carriage returns and replace with a <br>
+        currentBook = currentBook.replace(/(?:\r\n|\r|\n)/g, '<br>');
+
         document.getElementById("fileContent").innerHTML = currentBook;
 
         var elmnt=document.getElementById("fileContent");
@@ -26,4 +29,60 @@ function loadBook(filename,displayName){
 
     };
 
+}
+// get the stats for the book
+function getDocStats(fileContent){
+
+    var docLength = document.getElementById("doclength");
+    var wordCount = document.getElementById("wordCount");
+    var charCount = document.getElementById("charCount");
+
+    let text= fileContent.toLowerCase();
+    let wordArray = text.match(/\b\S+\b/g);
+    let wordDictionary = {};
+
+    //Count every word in wordArray
+    for( let word in wordArray){
+        let wordValue = wordArray[word];
+        if (wordDictionary[wordValue] > 0){
+            wordDictionary[wordValue] += 1; //incrementing count in dictionary
+        }
+        else {
+            wordDictionary[wordValue] = 1;
+        }
+    }
+
+    //sort the array
+    let worList = sortProperties(wordDictionary);
+
+    //Return the top 5 words
+    var top5Words = wordList.slice(0,6);
+    //return the least 5 words
+    var least5Words = wordList.slice(-6,wordList.length);
+
+    //Write the value to the page 
+
+}
+
+function ULTemplate(items,element){
+    let rowTemplate =document.getElementById('template-ul-items');
+    let templateHTML = rowTemplate.innerHTML;
+    let resultsHTML = "";
+
+    for(i=0;items.length-1;i++){
+        resultsHTML += templateHTML.replace('{{val}}', items[1][0]+ " : " + items[i][1] + "time(s)");
+    }
+
+    element.innerHTML = resultsHTML;
+}
+
+function sortProperties(obj){
+    //first convert the object to an array
+    let rtnArray = Object.defineProperties(obj);
+
+    //Sort the array
+    rtnArray.sort(function (first, second){
+        return second[1] - first[1];
+    });
+        return rtnArray;
 }
